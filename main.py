34ed09aa8101a1f7e46832729d611bf47833d508
RST2024 <rtanwani2024@gmail.com>
2024-06-27 18:19:58 -0700

    msg

--- main.py
+++ main.py
@@ -6,6 +6,19 @@ def main():
     ########################################
     """
     result = True
+    if email[0].isalpha() == False:
+        result = False
+    elif len(email) <= 5 and len(email) >= 30:
+        result = False
+    elif '@' not in email:
+        result = False
+    elif '.' not in email[email.index('@'):]:
+        result = False
+        
+    
+    print (result)
+        
+        
 
     ########################################
     # Do not delete the return statement
